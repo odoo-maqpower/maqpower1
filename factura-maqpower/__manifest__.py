8515b5bc95493ecb5c3f1b4c1fb4aaae87b3eed7
odoo-maqpower <69205865+odoo-maqpower@users.noreply.github.com>
2020-11-04 20:21:03 -0600

    Add files via upload

--- factura-maqpower/__manifest__.py
+++ factura-maqpower/__manifest__.py
@@ -4,9 +4,9 @@
     'name': 'FacturaMaqpower',
     'summary': 'Formato de factura integrado a FEL mexico enterprise',
     'version': '1.0',
-    'author': 'Tillit Asesoria',
-    'maintainer': 'Tillit Asesoria',
-    'website': 'Tillit Asesoria',
+    'author': 'Steve Piñero (Ogsistemas)',
+    'maintainer': 'OgSistemas',
+    'website': 'http://www.ogsistemas.com',
     'category': 'Hidden',
     'depends': ['base', 'l10n_mx_edi'],
     'data': [
--- factura-maqpower/models/__init__.py
+++ factura-maqpower/models/__init__.py
@@ -1,1 +1,4 @@
-# Hola
+# coding: utf-8
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import account_move
